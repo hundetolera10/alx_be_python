ae14bedc2941f8c6aa27419be7395553381b605f
Hunde Tolera <hundetolera225@gmail>
2025-09-12 00:08:38 +0300

    Add basic_operations.py for arthimetic exercise

---
+++ alx_be_python/basic_operations.py
@@ -0,0 +1,13 @@
+
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print results explicitly (one per line)
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
